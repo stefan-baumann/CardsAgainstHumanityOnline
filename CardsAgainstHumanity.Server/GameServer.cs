6e484e8ce7fda3b773a032195b2bbc83a0ccd8a3
Stefan Baumann <stefan-baumann@outlook.com>
2016-12-25 17:11:27 +0100

    Added more functionality to the web server

--- CardsAgainstHumanity.Server/GameServer.cs
+++ CardsAgainstHumanity.Server/GameServer.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CardsAgainstHumanity.Server
@@ -38,17 +39,17 @@ namespace CardsAgainstHumanity.Server
             if (string.IsNullOrWhiteSpace(requestTarget))
             {
                 path = new[] { "/" };
-                Console.WriteLine($"Received request for /");
+                //Console.WriteLine($"Received request for /");
             }
             else
             {
                 path = requestTarget.Split('/');
-                Console.WriteLine($"Received request for {requestTarget}.");
+                //Console.WriteLine($"Received request for {requestTarget}.");
             }
             
             if (this.ProcessRequestInternal(path, context))
             {
-                Console.WriteLine($"Processed request for {requestTarget ?? "/"}.");
+                //Console.WriteLine($"Processed request for {requestTarget ?? "/"}.");
             }
             else
             {
@@ -62,24 +63,36 @@ namespace CardsAgainstHumanity.Server
             switch (path[0])
             {
                 case "/":
-                    this.ProcessHomeRequest(context);
+                    this.ProcessHomeSiteRequest(context);
                     return true;
                 case "test":
-                    this.ProcessTestRequest(context);
+                    this.ProcessTestSiteRequest(context);
                     return true;
                 case "create":
-                    this.ProcessCreateGameRequest(context);
+                    Regex createRegex = new Regex(@"creategame\?name=(?<Name>\w+)&pass=(?<Password>\w+)");
+                    if (path.Length == 2 && createRegex.IsMatch(path[1]))
+                    {
+                        Match match = createRegex.Match(path[1]);
+                        this.ProcessCreateGameRequest(context, match.Groups["Name"].Value, match.Groups["Password"].Value);
+                    }
+                    else
+                    {
+                        this.ProcessCreateGameSiteRequest(context);
+                    }
                     return true;
                 case "join":
-                    this.ProcessJoinGameRequest(context);
+                    this.ProcessJoinGameSiteRequest(context);
                     return true;
+                case "favicon.ico":
+                    return true; //Just swallow
                 default:
                     return false;
             }
         }
 
-        protected void ProcessHomeRequest(HttpListenerContext context)
+        protected void ProcessHomeSiteRequest(HttpListenerContext context)
         {
+            Console.WriteLine($"Delivering main page to {context.Request.UserHostAddress}...");
             string response = $@"<html>
     <head>
         <title>Cards Against Humanity Online</title>
@@ -100,8 +113,9 @@ namespace CardsAgainstHumanity.Server
             context.WriteString(response);
         }
 
-        protected void ProcessJoinGameRequest(HttpListenerContext context)
+        protected void ProcessJoinGameSiteRequest(HttpListenerContext context)
         {
+            Console.WriteLine($"Delivering 'join game'-page to {context.Request.UserHostAddress}...");
             IEnumerable<Game> games = new[]
             {
                 new Game() { Id = 1, Name = "Test-Game #1", Players = new List<Player>() { new Player(), new Player() } },
@@ -126,18 +140,31 @@ namespace CardsAgainstHumanity.Server
             context.WriteString(response);
         }
 
-        protected void ProcessCreateGameRequest(HttpListenerContext context)
+        protected void ProcessCreateGameSiteRequest(HttpListenerContext context)
         {
+            Console.WriteLine($"Delivering 'create game'-page to {context.Request.UserHostAddress}...");
             string response = $@"<html>
     <head>
         <title>Cards Against Humanity Online - Create Game</title>
+        <script language='JavaScript'>
+            function createGame() {{
+                var request = new XMLHttpRequest();
+                request.onreadystatechange = function() {{
+                    if (request.readyState == 4 && request.status == 200) {{
+                        window.location.href = ""/../game/"" + request.responseText;
+                    }}
+                }}
+                request.open(""GET"", ""/create/creategame?name="" + namebox.value + ""&pass="" + passwordbox.value, true);
+                request.send(null);
+            }}
+        </script>
     </head>
     <body>
         <h1>Cards Against Humanity Online - Create Game</h1>
         <p>Create a new game with the specified name and password.</p>
-        <p>Name: <input id='name'></input></p>
-        <p>Password: <input id='password'></input></p>
-        <p><button onclick=''>Create Game</button></p>
+        <p>Name: <input id='namebox'></input></p>
+        <p>Password: <input id='passwordbox'></input></p>
+        <p><button onclick='createGame()'>Create Game</button></p>
         <br>
         <p>Copyright 2016 © Stefan Baumann (<a href='https://github.com/stefan-baumann'>GitHub</a>)</p>
         <p>This web game is based off the card game <a href='https://www.cardsagainsthumanity.com/'>Cards Against Humanity</a> which is available for free under the <a href='https://www.cardsagainsthumanity.com/'>Creative Commons BY-NC-SA 2.0 license</a>.
@@ -146,8 +173,10 @@ namespace CardsAgainstHumanity.Server
             context.WriteString(response);
         }
 
-        protected void ProcessTestRequest(HttpListenerContext context)
+        protected void ProcessTestSiteRequest(HttpListenerContext context)
         {
+            Console.WriteLine($"Delivering the test-page to {context.Request.UserHostAddress}...");
+
             BlackCard blackCard = this.CardDatabase.GetBlackCard();
             IEnumerable<WhiteCard> whiteCards = Enumerable.Repeat(1, 10).Select(o => this.CardDatabase.GetWhiteCard());
 
@@ -164,6 +193,14 @@ namespace CardsAgainstHumanity.Server
     </body>
 </html>";
             context.WriteString(response);
+        }
+
+
+
+        protected void ProcessCreateGameRequest(HttpListenerContext context, string name, string password)
+        {
+            Console.WriteLine($"{context.Request.UserHostAddress} created a new game with name '{name}' and password '{password}'.");
+            context.WriteString("1"); //Dummy-game with id 1
         }
     }
 }
